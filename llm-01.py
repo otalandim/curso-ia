060b0d308cb450418ab9a19db97c5b3ad25dc87a
Otavio Landim <otavio.landim@gmail.com>
2026-08-07 15:19:09 -0300

    Adicionando arquivo llm-01 com exemplo usando o Groq

---
+++ llm-01.py
@@ -0,0 +1,15 @@
+from groq import Groq
+
+client = Groq()
+
+response = client.chat.completions.create(
+    model="llama-3.1-8b-instant",
+    messages=[
+        {"role": "system", "content": "Atue como um especialista em machine learning"},
+        {"role": "user", "content": "De forma simples, o que é machine learning"},
+    ],
+    temperature=0,
+    top_p=1,
+)
+
+print(response.choices[0].message.content)
