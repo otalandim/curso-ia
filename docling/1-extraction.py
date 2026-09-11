16594abc3f6932004ed8b1cc71f3f2e77aa3a0b8
Otavio Landim <otavio.landim@gmail.com>
2026-09-11 11:48:34 -0300

    refactor: remove comments

--- docling/1-extraction.py
+++ docling/1-extraction.py
@@ -7,13 +7,3 @@ document = result.document
 markdown_output = document.export_to_markdown()
 
 print(markdown_output)
-
-# medo de falar em publico?
-# quem é você, o que você faz e como você faz
-
-
-# ler e matematica
-# jogos, ingles, historia (jogo timeline)
-# geo mundo
-# historia, geografia e ingles ensinar por meio de jogos
-# perfil de video educativo
